38275f2fcf571fd7777006f833feb01c5ac2c5ce
sirmd <mate.deitos@hotmail.com>
2020-08-03 14:31:00 -0300

    Criação de rotas e iniciando página de Professores

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -6,6 +6,7 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
     return (
@@ -21,14 +22,14 @@ const Landing = () => {
                 />
 
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar" />
                         Estudar
-                    </a>
-                    <a href="" className="give-classes">
+                    </Link>
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt="Dar Aulas" />
                         Dar Aulas
-                    </a>
+                    </Link>
                 </div>
                 <span className="total-connections">
                     Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import './styles.css';
+
+const TeacherForm = () => {
+    return (
+        <h1>TeacherForm</h1>
+    );
+}
+
+export default TeacherForm;
