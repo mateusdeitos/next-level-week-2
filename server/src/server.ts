12d67b6f14d999de7f411524b9224200c0ae5925
sirmd <mate.deitos@hotmail.com>
2020-08-04 14:18:10 -0300

    Cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 /* eslint-disable no-console */
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
