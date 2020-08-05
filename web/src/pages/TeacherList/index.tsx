531c829f19ac7ec5e97d33a5f2eb91a131e23006
sirmd <mate.deitos@hotmail.com>
2020-08-05 15:26:05 -0300

    Criação da página de Dar Aulas

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './styles.css';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 const TeacherList = () => {
     return (
@@ -10,18 +11,9 @@ const TeacherList = () => {
             <PageHeader title="Estes são os proffys disponíveis.">
 
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Assunto</label>
-                        <input type="text" id="subject" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="week-day">Dia da semana</label>
-                        <input type="text" id="week-day" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
-                    </div>
+                    <Input label="Matéria" name="subject"/>
+                    <Input label="Dia da semana" name="week_day"/>
+                    <Input type="time" label="Hora" name="time"/>                    
                 </form>
 
             </PageHeader>
